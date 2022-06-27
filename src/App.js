6c5ccd7728c4aee6a581fc58d7e2e2e5a101cf90
Allison Ruiz <97488383+allisonruiz@users.noreply.github.com>
2022-06-26 17:54:13 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm'
+
+
+function App(){
+    let [colors, setColors]  = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color ={color} key={i}/>
+        )
+    })
+    return (
+        <div className="App">
+            {colors.map((color, i) => 
+             <ColorBlock color ={color} key={i}/>
+            )}
+            <ColorForm addColor={addColor}/>
+        </div>
+    )
+}
+
+export default App;
